d3f1abe69cc4ff32b8124bd6c21fa9814803745e
Justin Calma <calma.justin.00@gmail.com>
2020-10-15 10:34:16 -0700

    Header file for the Digital class

--- Digital.h
+++ Digital.h
@@ -6,8 +6,10 @@
 //Class Digital is created from classes Publication and Sales
 //In this class you need to declare two functions getData and putData and a data member for storage
 
+// Create a class Digital and it is a child of classes Publication and Sales
  class Digital : public Publication, Sales {
 	
+	// Declare public methods and attribute
 	public:
 		void getData();
 		void putData();
