13d85225b5c5a64912259e69b2a01c706206a802
Justin Calma <calma.justin.00@gmail.com>
2020-10-15 10:33:22 -0700

    Implemenation file for the Publication class

--- Publication.cpp
+++ Publication.cpp
@@ -5,18 +5,23 @@ using namespace std;
 
 // Define the function getData to get title and price
 
+// Prompt the user to input the title and price 
 void Publication::getData() {
-	
+
+	// Prompt the user for the title	
 	cout << "Enter the title: " << endl;
 	getline(cin, title);
 	
+	// Prompt the user for the price
 	cout << "Enter the price: " << endl;
 	cin >> price;
 	
 }
 
+// Output to the user the Publication data
 void Publication::putData() {
 	
+	// Output the title and price 
 	cout << "The Title is: " << title << endl;
 	cout << "The Price is: " << price << endl;
 	
