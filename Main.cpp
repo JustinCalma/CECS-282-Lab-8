b82f9379141a7dc770f4d957497ca06545f65004
Justin Calma <calma.justin.00@gmail.com>
2020-10-15 10:33:55 -0700

    Main method for the Multiple Inheritance Program

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,6 @@
+// Justin Calma
+// CECS 282 Lab 8
+
 #include "Sales.h"
 #include "Publication.h"
 #include "Book.h"
@@ -6,24 +9,26 @@
 #include <string>
 using namespace std;
 
+// Definition of the main method
 int main() {
 
+	// Create objects of type Book and Digital
     Book book;
-
     Digital digital;
 
+	// Get the data of object book from the user and output it 
     book.getData();
-
     book.putData();
 
     cout << endl;
 
+	// Get the data of object digital from the user and output it 
     digital.getData();
-
     digital.putData();
 
     cout << endl;
 
+	// Return 0 to main
     return 0;
 
 }
